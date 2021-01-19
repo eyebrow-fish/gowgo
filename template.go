4e98ad1764505c008041ab2c61811d96711d078f
Alexander Johnston <Aliics@hotmail.com>
2021-01-19 21:39:04 +1300

    path templating

--- template.go
+++ template.go
@@ -1,6 +1,7 @@
 package gowgo
 
 import (
+	"fmt"
 	"io/ioutil"
 	"os"
 	"regexp"
@@ -12,18 +13,33 @@ func RenderTemplate(input, output string, attr map[string]string) error {
 	if err != nil {
 		return err
 	}
-	varExp, err := regexp.Compile("{{\\s*var\\s*=\\s*[A-z0-9]+\\s*}}")
+	varMatches, err := findMatches("var", string(inputData))
 	if err != nil {
 		return err
 	}
-	varMatches := varExp.FindAllString(string(inputData), -1)
+	pathMatches, err := findMatches("path", string(inputData))
+	if err != nil {
+		return err
+	}
 	template := string(inputData)
 	for _, match := range varMatches {
 		key := strings.TrimFunc(
 			strings.SplitAfter(match, "=")[1],
 			func(r rune) bool { return r == ' ' || r == '}' },
 		)
-		template = strings.ReplaceAll(string(template), match, attr[key])
+		template = strings.ReplaceAll(template, match, attr[key])
+	}
+	for _, match := range pathMatches {
+		fileName := strings.TrimFunc(
+			strings.SplitAfter(match, "=")[1],
+			func(r rune) bool { return r == ' ' || r == '}' },
+		)
+		file, err := os.Stat(fileName)
+		if err != nil {
+			return err
+		}
+		parent := strings.Repeat("../", strings.Count(output, "/"))
+		template = strings.ReplaceAll(template, match, parent + file.Name())
 	}
 	paths := strings.SplitAfter(output, string(os.PathSeparator))
 	err = os.Mkdir(strings.Join(paths[:len(paths)-1], string(os.PathSeparator)), 0755)
@@ -42,4 +58,13 @@ func RenderTemplate(input, output string, attr map[string]string) error {
 	}
 	_, err = outputFile.WriteString(template)
 	return err
+}
+
+func findMatches(typeName, inputData string) ([]string, error) {
+	varExp, err := regexp.Compile(fmt.Sprintf("{{\\s*%s\\s*=\\s*[A-z0-9\\.]+\\s*}}", typeName))
+	if err != nil {
+		return nil, err
+	}
+	varMatches := varExp.FindAllString(inputData, -1)
+	return varMatches, nil
 }
