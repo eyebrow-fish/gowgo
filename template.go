5b507029aeba49dc4efbd7335db47828c187f7f3
Alexander Johnston <Aliics@hotmail.com>
2021-01-21 08:35:36 +1300

    pull out templating

--- template.go
+++ template.go
@@ -17,29 +17,15 @@ func RenderTemplate(input, output string, attr map[string]string) error {
 	if err != nil {
 		return err
 	}
+	template := string(inputData)
+	template = injectVars(template, attr, varMatches)
 	pathMatches, err := findMatches("path", string(inputData))
 	if err != nil {
 		return err
 	}
-	template := string(inputData)
-	for _, match := range varMatches {
-		key := strings.TrimFunc(
-			strings.SplitAfter(match, "=")[1],
-			func(r rune) bool { return r == ' ' || r == '}' },
-		)
-		template = strings.ReplaceAll(template, match, attr[key])
-	}
-	for _, match := range pathMatches {
-		fileName := strings.TrimFunc(
-			strings.SplitAfter(match, "=")[1],
-			func(r rune) bool { return r == ' ' || r == '}' },
-		)
-		file, err := os.Stat(fileName)
-		if err != nil {
-			return err
-		}
-		parent := strings.Repeat("../", strings.Count(output, "/"))
-		template = strings.ReplaceAll(template, match, parent + file.Name())
+	template, err = injectPaths(template, output, pathMatches)
+	if err != nil {
+		return err
 	}
 	paths := strings.SplitAfter(output, string(os.PathSeparator))
 	err = os.Mkdir(strings.Join(paths[:len(paths)-1], string(os.PathSeparator)), 0755)
@@ -58,6 +44,33 @@ func RenderTemplate(input, output string, attr map[string]string) error {
 	}
 	_, err = outputFile.WriteString(template)
 	return err
+}
+
+func injectPaths(template, output string, pathMatches []string) (string, error) {
+	for _, match := range pathMatches {
+		fileName := strings.TrimFunc(
+			strings.SplitAfter(match, "=")[1],
+			func(r rune) bool { return r == ' ' || r == '}' },
+		)
+		file, err := os.Stat(fileName)
+		if err != nil {
+			return "", err
+		}
+		parent := strings.Repeat("../", strings.Count(output, "/"))
+		template = strings.ReplaceAll(template, match, parent+file.Name())
+	}
+	return template, nil
+}
+
+func injectVars(template string, attr map[string]string, varMatches []string) string {
+	for _, match := range varMatches {
+		key := strings.TrimFunc(
+			strings.SplitAfter(match, "=")[1],
+			func(r rune) bool { return r == ' ' || r == '}' },
+		)
+		template = strings.ReplaceAll(template, match, attr[key])
+	}
+	return template
 }
 
 func findMatches(typeName, inputData string) ([]string, error) {
