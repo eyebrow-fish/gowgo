f91194edb65c9f8987c67761e807825390b089b5
Alexander Johnston <Aliics@hotmail.com>
2021-01-23 10:29:52 +1300

    internal "code" type

--- code.go
+++ code.go
@@ -7,42 +7,49 @@ import (
 	"strings"
 )
 
+type code string
+
 func ReadCode(filename string) (string, error) {
 	codeBytes, err := ioutil.ReadFile(fmt.Sprintf("cmd/%s/main.go", filename))
 	if err != nil {
 		return "", err
 	}
-	code := string(codeBytes)
-	code, err = highlightRegexAs(code, "\".*\"", "string")
+	c := code(codeBytes)
+	strs, err := c.highlightRegexAs("\".*\"", "string")
 	if err != nil {
 		return "", err
 	}
-	code, err = highlightRegexAs(code, "[0-9]+", "prim")
+	prims, err := strs.highlightRegexAs("([0-9]+)|true|false", "prim")
 	if err != nil {
 		return "", err
 	}
-	code = highlightKeyword(code, "package")
-	code = highlightKeyword(code, "import")
-	code = highlightKeyword(code, "func")
-	code = highlightKeyword(code, "var")
-	code = highlightKeyword(code, "const")
-	code = highlightKeyword(code, "if")
-	code = highlightKeyword(code, "else")
-	code = strings.TrimRight(code, "\n ")
-	return code, err
+	packages := prims.highlightKeyword("package")
+	imps := packages.highlightKeyword("import")
+	funcs := imps.highlightKeyword("func")
+	vars := funcs.highlightKeyword("var")
+	consts := vars.highlightKeyword("const")
+	ifs := consts.highlightKeyword("if")
+	elses := ifs.highlightKeyword("else")
+	trimmed := elses.removeLastLine()
+	return string(trimmed), err
 }
 
-func highlightKeyword(code, word string) string {
-	return strings.ReplaceAll(code, word, fmt.Sprintf(`<span class="keyword">%s</span>`, word))
+func (c code) highlightKeyword(word string) code {
+	return code(strings.ReplaceAll(string(c), word, fmt.Sprintf(`<span class="keyword">%s</span>`, word)))
 }
 
-func highlightRegexAs(code, regex, class string) (string, error) {
+func (c code) highlightRegexAs(regex, class string) (code, error) {
 	pattern, err := regexp.Compile(regex)
 	if err != nil {
 		return "", err
 	}
-	for _, s := range pattern.FindAllString(code, -1) {
-		code = strings.Replace(code, s, fmt.Sprintf(`<span class="%s">%s</span>`, class, s), 1)
+	var full string
+	for _, s := range pattern.FindAllString(full, -1) {
+		full = strings.ReplaceAll(full, s, fmt.Sprintf(`<span class="%s">%s</span>`, class, s))
 	}
-	return code, nil
+	return code(full), nil
+}
+
+func (c code) removeLastLine() code {
+	return code(strings.TrimRight(string(c), "\n "))
 }
